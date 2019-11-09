efbca90600c75969fee27259d29b80d3beebfe6c
Karl Matthes <karlmatthes@gmail.com>
2019-11-09 01:40:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,8 +19,8 @@ def load_library(filepath)
   emote_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(filepath, emoticon)
+  emoticon_library = load_library(filepath)
 end
 
 def get_english_meaning
